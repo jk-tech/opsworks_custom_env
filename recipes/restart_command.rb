7a9c91c5edbb8ddf864c4e9a9469437a42584fbc
James Miller <bensie@gmail.com>
2014-07-10 21:49:25 -0700

    Use default restart command, don't force unicorn config

--- recipes/restart_command.rb
+++ recipes/restart_command.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
 
   execute "restart Rails app #{application} for custom env" do
     cwd deploy[:current_path]
-    command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+    command node[:opsworks][:rails_stack][:restart_command]
     user deploy[:user]
 
     action :nothing
